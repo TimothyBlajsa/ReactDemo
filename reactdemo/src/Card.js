e576ca91f157917de45573491ca99a98de111097
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-02-23 15:02:21 -0500

    Move components to correct files

--- reactdemo/src/Card.js
+++ reactdemo/src/Card.js
@@ -1,3 +1,11 @@
 import React from "react";
 import style from "./card.module.css";
 import CardDetails from "./CardDetails";
+
+const Card = ({}) => {
+    return (
+        <div className="card-container"></div>
+    )
+};
+
+export default Card;
---
+++ reactdemo/src/CardDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './card.module.css';
+
+const CardDetails = ({}) => {
+    return (
+        
+          <div className="card-title">
+            <p className="card-title-text">Our Card</p>
+          </div>
+    )
+};
+export default CardDetails;
