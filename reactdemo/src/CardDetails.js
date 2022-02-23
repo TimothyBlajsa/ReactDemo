a4774c1fa51bf68fc984e55d0640c4c6994c6e48
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-02-23 15:25:08 -0500

    Add filler content for card

--- reactdemo/src/CardDetails.js
+++ reactdemo/src/CardDetails.js
@@ -4,9 +4,15 @@ import style from './card.module.css';
 const CardDetails = () => {
     return (
         
+        <>
           <div className={style.card_title}>
             <p className={style.card_title_text}>Our Card</p>
           </div>
+          <div className={style.card_content}>
+            <p className={style.card_content_text}> This is the content for this particular demonstration card
+            </p>
+          </div>
+        </>
     )
 };
 export default CardDetails;
