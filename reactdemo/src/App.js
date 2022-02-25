1b38b3fb448f96837bf851ba740c6ae5f05a62f3
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-02-25 18:21:13 -0500

    Update styling

--- reactdemo/src/App.js
+++ reactdemo/src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Card from './Card';
 
+function sayHello(){
+  alert("Hello");
+}
+
 function App() {
   return (
     <div className="App">
@@ -58,6 +62,12 @@ function App() {
         <p>1. Card Container </p>
         <p>2. Card Details </p>
         <Card></Card>
+
+        <h1>Let's make a button!</h1>
+        <p>1. Create a button the way you usually do</p>
+        <p>2. Set onClick within tag</p>
+        <p>3. Create a function to handle the onClick event</p>
+        <form onClick={sayHello}>Click Me!</form>
 
         <footer><a href='https://github.com/TimothyBlajsa/ReactDemo'>Investigate the source code!</a></footer>
       </header>
